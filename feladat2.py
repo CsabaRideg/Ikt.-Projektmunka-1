c1d19844b6335a8243b0b4673713deb259063a4a
CsabaRideg <116632163+CsabaRideg@users.noreply.github.com>
2024-10-05 17:16:42 +0200

    Add files via upload

--- feladat2.py
+++ feladat2.py
@@ -1,14 +1,13 @@
 import string
 import time
 ABC = string.ascii_letters
-ABC_numbered = [['a',1],['b',2],['c',3],['d',4],['e',5],['f',6],['g',7],['h',8],['i',9],['j',10],['k',11],['l',12],['m',13],['n',14],['o',15],['p',16],['q',17],['r',18],['s',19],['t',20],['u',21],['v',22],['w',23],['x',24],['y',25],['z',26]]
 NUM = [0,1,2,3,4,5,6,7,8,9]
 
 f = open('ki.txt','r',encoding='utf8')
 lines = f.readlines()
 f.close()
 
-def arrangement(list):
+def arrangement(list): #cserés rendezés
     for count,word in enumerate(list):
         if count != 0:
             current = count
@@ -35,57 +34,40 @@ def arrangement(list):
                         list[before],list[current] = list[current],list[before]
                     current -=1
                     before-=1
+    return list
 
-def arrangement2(list):
-    print(len(list))
-    for count,word in enumerate(list):
-        print()
-        print(f"Kiválasztott elem: {word}, {count}")
-        print(list)
-        word = word.lower()
-        if count != 0:
-            if word < list[count-1].lower() or word > list[count+1].lower():
-                for letter in ABC_numbered:
-                    if word[0].lower() == letter[0]:
-                        list.insert(int(len(list)/26*(letter[1])),word)
-                        list.remove(list[count])
-            else:
+def arrangement2(list): #Shell rendezés
+    max_square = 2
+    square= 2
+    while True:
+        if max_square < len(list)/2:
+            max_square = int(max_square *2)
+        if max_square > len(list)/2:
+            max_square = int(max_square/2)
+            break
+    while max_square >= square:
+        for count,word in enumerate(list):
+            test_count = count
+            gap = int(len(list)/square)
+            little_list = []
+            while len(little_list) != square:
+                little_list.append(list[test_count])
+                test_count += gap
+                if test_count >= len(list):
+                    break
+            test_count = count
+            arrangement(little_list)
+            while len(little_list) != 0:
+                list[test_count] = little_list[0]
+                little_list.remove(little_list[0])
+                test_count += gap
+            if count == gap:
+                square = int(square*2)
                 break
-            current = list.index(word)
-            before = current-1
-            after = current + 1
-            print(f"Az elem új indexe: {current}")
-            while word < list[before] or word > list[after]:
-                print(f"Az elötte lévő nagyobb: '{word < list[before]}', Az utána lévő kisebb '{word > list[after]}'")
-                print(f"{word} a szó, elöte: {list[before]}, utána: {list[after]}")
-                print(f"{list}")
-                if before == 0:
-                    print(f"lista Eleje")
-                    list[before],word = word,list[before]
-                    break
-                elif after == len(list)-1:
-                    print(f"lista vége")
-                    list[after],word = word,list[after]
-                    break
-                if word > list[after] and word < list[before]:
-                    print("Helyén van")
-                    break
-                if word < list[before]:
-                    print("elotte")
-                    list[before],word= word,list[before]
-                    current -= 1
-                    before -= 1
-                    after -= 1
-                elif word > list[after]:
-                    print("utana")
-                    list[after],word= word,list[after]
-                    current +=1
-                    before +=1
-                    after += 1
-                time.sleep(0.1)
+    arrangement(list)
+    return list       
 
-numbers = []
-words = []
+items = []
 correct_chars = True
 for line in lines:
     line = line.strip().split(";")
@@ -103,20 +85,21 @@ for line in lines:
                     if int(char) not in NUM:
                         correct_chars = False
         if Type =='text':
-            words.append(item)
+            items.append(item)
         elif Type =='number':
-            numbers.append(int(item))
+            items.append(int(item))
 
 if correct_chars == False:
     print('Helytelen adathalmaz.')
-sequence = int(input('Növekvő = 1, Csökkenő = 0: '))
-if Type == 'number':
-    arrangement(numbers)
-    if sequence == 0:
-        numbers = numbers[::-1]
-        print(numbers)
-else:
-    arrangement2(words)
-    if sequence == 0:
-        words = words[::-1]
-        print(words)
+
+arrangement_type = int(input('Melyik rendezési módszerrel legyen rendezve a list? 1/2 -- '))
+if arrangement_type == 1:
+    list_arranged = arrangement(items)
+elif arrangement_type == 2:
+    list_arranged = arrangement2(items)
+
+sequence = int(input('Növekvő vagy Csökkenő legyen a sorrend? 1/-1 -- '))
+if sequence == -1:
+    print(list_arranged[::-1])
+if sequence == 1:
+    print(list_arranged)
