399ddd8fcdbb642bbf848e987818c66e077b74de
Rideg Csaba <ridegslug@gmail.com>
2024-10-10 09:47:04 +0200

    1010_pygame_start

--- Sorting.py
+++ Sorting.py
@@ -1,11 +1,27 @@
 import pygame
-runnging = True
+py = pygame
+
+width,height = 1980,1080
+
+py.init()
+screen = py.display.set_mode((0,0),py.FULLSCREEN)
+screen.fill(((66, 66, 88)))
+py.display.set_caption('Rendezés')
+
+font_1 = py.font.Font('fonts/Cocogoose-Pro-Light.ttf', 100)
+main_text =  font_1.render('Lista rendezés', True, 'black')
+main_text_rect = main_text.get_rect(center=(width/2,55))
+
+sort_mode_btn = pygame.Surface((300,100))
+sort_mode_btn_rect = sort_mode_btn.get_rect(center=(width/2,height/2))
+
+running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == py.QUIT:
             running = False
 
 
-
-    pygame.display.update()
-pygame.quit()
+    screen.blit(main_text,main_text_rect)
+    py.display.update()
+py.quit()
