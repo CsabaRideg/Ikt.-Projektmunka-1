f72cdcbf47a3fc484c9e00f826fabf76d5d7ad55
Rideg <ridegslug@gmail.com>
2024-10-12 13:15:21 +0200

    1012_mixed_lists

--- feladat2.py
+++ feladat2.py
@@ -9,36 +9,24 @@ if len(lines) > 1:
     if arrange_together == 0:
         lines = ["".join(lines)]
 
-def arrangement(list,data_type): #Cserés rendezés
+def arrangement1(list): #Cserés rendezés
     for count,word in enumerate(list):
         if count != 0:
             current = count
             before = count-1
-            if data_type == 'szöveg':
-                while list[current].upper() < list[before].upper():
-                    if list[current].upper() > list[before].upper():
-                        break
-                    if before == 0:
-                        list[before],list[current] = list[current],list[before]
-                        break
-                    if list[current].upper() < list[before].upper():
-                        list[before],list[current] = list[current],list[before]
-                    current -=1
-                    before-=1
-            else:
-                while list[current] < list[before]:
-                    if list[current] > list[before]:
-                        break
-                    if before == 0:
-                        list[before],list[current] = list[current],list[before]
-                        break
-                    if list[current] < list[before]:
-                        list[before],list[current] = list[current],list[before]
-                    current -=1
-                    before-=1
+            while list[current].upper() < list[before].upper():
+                if list[current].upper() > list[before].upper():
+                    break
+                if before == 0:
+                    list[before],list[current] = list[current],list[before]
+                    break
+                if list[current].upper() < list[before].upper():
+                    list[before],list[current] = list[current],list[before]
+                current -=1
+                before-=1
     return list
 
-def arrangement2(list,data_type): #Shell rendezés
+def arrangement2(list): #Shell rendezés
     max_square = 2
     square= 2
     while True:
@@ -58,7 +46,7 @@ def arrangement2(list,data_type): #Shell rendezés
                 if test_count >= len(list):
                     break
             test_count = count
-            arrangement(little_list,data_type)
+            arrangement1(little_list)
             while len(little_list) != 0:
                 list[test_count] = little_list[0]
                 little_list.remove(little_list[0])
@@ -66,20 +54,36 @@ def arrangement2(list,data_type): #Shell rendezés
             if count == gap:
                 square = int(square*2)
                 break
-    arrangement(list,data_type)
+    arrangement1(list)
     return list       
 
-items = []
-list_types = []
+def number_converter(list): #Számok elé 0-kat pakolás  
+    numbers = []
+    for item in list:
+        if item.isnumeric():
+            numbers.append(int(item))
+    length = len(str(max(numbers)))
+    numbers = [str(i) for i in numbers]
+    for count,number in enumerate(numbers):
+        list.remove(number)
+        if len(number) != length:
+            numbers[count] = (length-len(number))*'0' + number
+    list = numbers + list
+    return list
+
+def number_reverter(list): # Számok elöl 0-kat eltöntetés
+    for count,item in enumerate(list):
+        if item.isnumeric():
+            list[count] = str(int(item))
+    return list
+
+lists = []
 correct_list = True
 for count,line in enumerate(lines):
     if all(i.isnumeric() for i in lines[count]):
-        line = [int(i) for i in line]
-        items.append(line)
-        list_types.append('szám')
+        lists.append(number_converter(line))
     elif all(i.isalpha() for i in lines[count]):
-        items.append(line)
-        list_types.append('szöveg')
+        lists.append(line)
     else:
         for item in line:
             if all(i.isnumeric() for i in item)==False and all(i.isalpha() for i in item)==False:
@@ -87,8 +91,8 @@ for count,line in enumerate(lines):
                 exit()
         mixed_list = int(input(f"A {count+1}. lista tartalmaz számokat és betühalmazokat is. Egybe vagy kölön a számokat és külön a betűhalmazokat rendezve? 0/1 -- "))
         if mixed_list == 0:
-            list_types.append('vegyes')
-            items.append(line)
+
+            lists.append(number_converter(line))
         elif mixed_list == 1:
             numbers= []
             words = []
@@ -97,21 +101,28 @@ for count,line in enumerate(lines):
                     numbers.append(item)
                 elif all(i.isalpha() for i in item):
                     words.append(item)
-            list_types.append('külön')
-            items.append([numbers,words])
-print(list_types)
-print(items)
+            lists.append([number_converter(numbers),words])
+print(lists)
 
 arrangement_type = int(input('Melyik rendezési módszerrel legyen rendezve a lista? 1/2 -- '))
-if arrangement_type == 1:
-    list_arranged = arrangement(items[0],list_types[0])
-elif arrangement_type == 2:
-    list_arranged = arrangement2(items[0],list_types[0])
-
 sequence = int(input('Növekvő vagy Csökkenő legyen a sorrend? 1/-1 -- '))
-print(list_arranged[::sequence])
+for count,i in enumerate(lists):
+    if len(i) ==2:
+        if isinstance(i[1],list):
+            if arrangement_type == 1:
+                all(lists[count][count2] == arrangement1(i2)[::sequence] for count2,i2 in enumerate(i))
+            elif arrangement_type == 2:
+                all(lists[count][count2] == arrangement2(i2)[::sequence] for count2,i2 in enumerate(i))
+    else:
+        if arrangement_type == 1:
+            lists[count] == arrangement1(i)[::sequence]
+        elif arrangement_type == 2:
+            lists[count] == arrangement2(i)[::sequence]
 
+for i in lists:
+    print(i)
 add_new = int(input(f"Szeretne új elemet hozzáadni a listához? 0/1 -- "))
+list_number = int()
 while add_new != 0:
     new_item = input(f"Adjon meg egy új elemet. -- ")
     for char in new_item:
@@ -119,8 +130,8 @@ while add_new != 0:
             print(f"Az elem minden karaktere kell hogy legyen!")
         if type != 'number' and char.isnumeric():
             print(f"Az elem minden karaktere kell hogy legyen!")
-    for count,i in enumerate(items[0]):
+    for count,i in enumerate(lists[0]):
         if i.upper() > new_item.upper():
-            items.insert(count,new_item)
+            lists.insert(count,new_item)
             break
     add_new = int(input(f"Szeretne új elemet hozzáadni a listához? 0/1 -- "))
